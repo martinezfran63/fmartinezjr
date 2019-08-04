271aa8100588608c3895ea408ab6e42f5237e7d6
Francisco <fmartinez@weedmaps.com>
2019-08-04 11:44:32 -0600

    set up landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Typing from 'react-typing-animation';
- 
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Red+Hat+Text&display=swap');
+    font-family: 'Red Hat Text', sans-serif;
+  }
+  `
+
+const H1 = styled.h1`
+font-weight: bold;
+color: black;
+font-size: 6em
+margin: 0.5rem 1rem;
+`;
+
 
 function App() {
   return (
-   <Typing>
-    <span>francisco </span>
-    <span>martinez </span>
-    <span>jr</span>
+   <Typing speed={50}>
+     <GlobalStyles />
+    <H1>Francisco Martinez Jr</H1>
   </Typing>
   );
 }
 
 export default App;
+
 
