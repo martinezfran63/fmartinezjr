64755b3002a9d3ceb2000f2cf7907c6beff7a1bc
Francisco <fmartinez@weedmaps.com>
2019-08-04 12:26:49 -0600

    breadcrumbs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import Typing from 'react-typing-animation';
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  paper: {
+    padding: theme.spacing(1, 2),
+  },
+}));
+
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -19,11 +35,39 @@ margin: 0.5rem 1rem;
 
 
 function App() {
+  const classes = useStyles();
   return (
+    <div>
+
+
    <Typing speed={50}>
      <GlobalStyles />
     <H1>francisco martinez jr</H1>
   </Typing>
+
+  <div className={classes.root}>
+      <Paper elevation={0} className={classes.paper}>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" href="#career" >
+          Career       
+          </Link>
+          <Link color="inherit" href="#contact">
+            Contact
+          </Link>
+          <Link color="inherit" href="#portfolio">
+            Portfolio
+          </Link>
+          <Link color="inherit" href="#interests">
+            Interests
+          </Link>
+        </Breadcrumbs>
+      </Paper>
+
+      
+    </div>
+
+  </div>
+
   );
 }
 
