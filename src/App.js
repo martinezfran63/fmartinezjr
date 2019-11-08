9652a330abd819f8b1ca529bd3f57e86ab4315ca
Francisco Martinez <fmartinez@Franciscos-MacBook-Pro.local>
2019-11-08 13:46:35 -0700

    add rinconcito menu page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Career from "./career.js";
 import Contact from "./contact.js";
 import Homepage from "./homepage.js";
 import Portfolio from "./portfolio.js";
+import Rinconcito_Menu from "./rinconcito_menu.js";
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 
@@ -27,6 +28,7 @@ function App() {
           <Route path="/career" component={Career} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
+          <Route path="/rinconcito_menu" component={Rinconcito_Menu} />
         </Switch>
       </Router>
     </div>
