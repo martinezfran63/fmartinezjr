98a6ec9f9318b7c4cbb14d3a5b93a3b1040ca747
martinezfran63 <martinezfran63@gmail.com>
2019-12-13 14:31:26 -0700

    Updates

---
+++ precache-manifest.fb9118008139858ce9e89b8c71d8a9a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb94cd426e684124cdbf82b36d48b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d88642e0930b78656db",
+    "url": "/static/js/2.1108afca.chunk.js"
+  },
+  {
+    "revision": "23bb4917ae788fc4480d",
+    "url": "/static/js/main.2f049213.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6af2daeefe95585be817d3453ba41c1",
+    "url": "/static/media/AC&DP.c6af2dae.png"
+  },
+  {
+    "revision": "fff26fd52b224eee105f8021e4dfdb6e",
+    "url": "/static/media/Francisco_Martinez.fff26fd5.pdf"
+  },
+  {
+    "revision": "0221a9fc59db2b5697888b90ad0b390a",
+    "url": "/static/media/linkedin.0221a9fc.png"
+  },
+  {
+    "revision": "e4e99b93b6fa411e771be6ec3f0855a9",
+    "url": "/static/media/menu_1_frutuas_preparadas.e4e99b93.jpg"
+  },
+  {
+    "revision": "c3624fb14aac85bf58c0b9c54b9fc6f5",
+    "url": "/static/media/rinconcito_logo_big.c3624fb1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fmartinezjr/precache-manifest.03f900b1ee2dcf508a6e92d540ddd339.js"
+  "/precache-manifest.fb9118008139858ce9e89b8c71d8a9a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fmartinezjr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
